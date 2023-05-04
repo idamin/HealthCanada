79e7057c0ebdc577a452db21bee8f37a2bb1205d
Ishan Amin <ishanamin21@gmail.com>
2023-05-04 19:03:16 -0400

    Created get_data function to pull relevant information from API

--- script.py
+++ script.py
@@ -1,7 +1,28 @@
 import requests
 import json
 import cred
+import pandas as pd
 from openpyxl import Workbook, load_workbook
+
+
+#-------------------Helper Functions-------------------
+'''
+Obtains Manufacturer, Product, Notice of Compliance Date, and the Medical Ingridients
+from the Health Canada API given the Drug Information Number (DIN).
+
+Note that the NOC Database and the DGD database have different nomenclature
+for the above terms, the code below follows the NOC database. 
+'''
+
+def get_data(din):
+   response =  requests.get('https://node.hres.ca/drug/product?key=' + cred.API_KEY + '&search=drug_identification_number:' + din).json()
+   manufacturer = response['results'][0]['_source']['company']['company_name']  
+   product = response['results'][0]['_source']['brand_name']
+   NOCDate = response['results'][0]['_source']['status_approved_date'][0][:10]
+   MedicalIngredients = response['results'][0]['_source']['active_ingredients']
+   return[product,manufacturer,NOCDate,MedicalIngredients]
+#------------------------------------------------------
+#get_test = get_data('02537532')
 
 wb = load_workbook('test_file1.xlsx')
 ws = wb.active
@@ -10,6 +31,6 @@ din_list = []
 for dins in ws['A']: 
     din_list.append(dins.value)
 
-print(din_list)
+din_list = din_list[1:]
 
-#response = requests.get('https://node.hres.ca/drug/product?key=' + cred.API_KEY + '&search=drug_identification_number:' + '02537648' )
+
